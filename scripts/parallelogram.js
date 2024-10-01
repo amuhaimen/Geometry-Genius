a232aa9b7793da563c283ac2de29b058f31aa2f7
sm-muhaimen <amuhaimenpro829@gmail.com>
2024-10-01 18:42:49 +0600

    calculate parallelogram done

--- scripts/parallelogram.js
+++ scripts/parallelogram.js
@@ -1,1 +1,18 @@
-console.log("connected");
+function calculateParallelogram() {
+  const base = getInputValueById("Parallelogram_base");
+  const height = getInputValueById("Parallelogram_height");
+  const area = base * height;
+  setInnerTextById("Parallelogram_area", area);
+}
+
+function getInputValueById(inputFieldId) {
+  const inputField = document.getElementById(inputFieldId);
+  const inputText = inputField.value;
+  const inputValue = parseFloat(inputText);
+  return inputValue;
+}
+
+function setInnerTextById(elementId, area) {
+  const element = document.getElementById(elementId);
+  element.innerText = area;
+}
